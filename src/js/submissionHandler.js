385c72870600c6cf1f059497e2c31829acdc606b
Jonathan Garrett <jonathan.garrett@reasondigital.com>
2017-12-02 20:03:25 +0000

    Add Vue.js and development version, create logic process for form submission handler and data submission

--- src/js/submissionHandler.js
+++ src/js/submissionHandler.js
@@ -9,38 +9,95 @@
  *
  */
 
-let DATA = {
-	personal_1: 'yes',
-	personal_2: 'no',
-	personal_3: 'Cheshire',
-	personal_4: '17-24',
-	personal_5: '12+',
-	personal_6: '20000',
-};
+window.firebase.apikey = API_KEY;
+window.firebase.projectId = PROJECT_ID;
+window.firebase.collection = COLLECTION_NAME;
 
-firebase.initializeApp({
-	apiKey: API_KEY,
-	authDomain: `${PROJECT_ID}.firebaseapp.com`,
-	projectId: PROJECT_ID
+new Vue({
+	el: "#survey-app",
+	data: {
+		db: null,
+		dbCollection: window.firebase.collection,
+		dbApiKey: window.firebase.apikey,
+		dbProjectId: window.firebase.projectId,
+		validating: false,
+		successfulSubmission: false
+	},
+	methods: {
+		initDB() {
+			const firebaseCredentials = {
+				apiKey: `${this.dbApiKey}`,
+				authDomain: `${this.projectId}.firebaseapp.com`,
+				projectId: `${this.projectId}`
+			};
+
+			firebase.initializeApp(firebaseCredentials);
+			this.db = firebase.firestore();
+
+		},
+		listSubmissions() {
+			if (this.db !== null) {
+				this.db.collection(this.dbCollection).get()
+					.then(querySnapshot => {
+						querySnapshot.forEach(submission => {
+							console.log(submission.data());
+						})
+					})
+					.catch(error => {
+						console.log(`Error: ${error}`);
+					});
+			} else {
+				console.log('No DB Connection');
+			}
+		},
+		addSubmission(jsonCollectionData) {
+			if (this.db !== null) {
+				this.db.collection(this.dbCollection).add(jsonCollectionData)
+					.then(submissionReference => {
+						this.successfulSubmission = true;
+						// TODO: Remove after debugging
+						console.log(`Added: ${submissionReference.id}`);
+						this.listSubmissions();
+					})
+					.catch(error => {
+						console.log(`Error: ${error}`);
+					})
+			} else {
+				console.log('No DB Connection');
+			}
+		},
+		buildSubmissionData() {
+			return {
+				personal_1: 'yes',
+				personal_2: 'no',
+				personal_3: 'Cheshire',
+				personal_4: '17-24',
+				personal_5: '12+',
+				personal_6: '20000',
+			};
+		},
+		onPrimarySubmission() {
+			console.log('primary submission clicked');
+			if (this.validate()) {
+				const jsonSubmissionData = this.buildSubmissionData();
+				this.addSubmission(jsonSubmissionData);
+			}
+		},
+		onSecondarySubmission() {
+			console.log('secondary submission clicked');
+		},
+		validate() {
+			this.validating = true;
+			let valid = false;
+
+			// TODO: Validate user inputs
+			valid = true;
+
+			this.validating = false;
+			return valid;
+		}
+	},
+	created() {
+		this.initDB();
+	}
 });
-const db = firebase.firestore();
-
-const showSubmissions = () => {
-	db.collection(COLLECTION_NAME).get()
-		.then(querySnapshot => {
-			querySnapshot.forEach(submission => {
-				console.log(submission.data());
-			})
-		});
-}
-
-showSubmissions()
-
-// db.collection(COLLECTION_NAME).add(DATA)
-// 	.then(submissionReference => {
-// 		console.log(`Added: ${submissionReference.id}`);
-// 		showSubmissions();
-// 	})
-// 	.catch(error => {
-// 		console.log(`Error: ${error}`);
-// 	})
